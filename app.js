1a1f0954d38483e7aeba23b45f28ce52fd006c20
Adnan Ali <adnanali6688@gmail.com>
2022-11-17 15:21:02 +0100

    implimented CRUD for users

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 
 const fs = require('fs');
+const morgan = require('morgan');
 
 app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/users.json`)
 );
 
 /////////////////////////// Middleware //////////////////////////
@@ -23,6 +28,13 @@ app.use((req, res, next) => {
   req.reuestedBy = 'Adnan Ali';
   next();
 });
+
+// Third party middleware
+app.use(morgan('dev')); // it will give us inofrmation regarding request, status code etc
+
+///////////////////////////////////////////////////////////////////
+// Routes implimentation for 'TOURS' routes
+///////////////////////////////////////////////////////////////////
 
 /////////////////////////////// GET Method //////////////////////////////////
 
@@ -137,6 +149,131 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     tours: { message: 'Invalid Id' },
   });
 });
+
+///////////////////////////////////////////////////////////////////
+// Routes implimentation for 'USERS' routes
+///////////////////////////////////////////////////////////////////
+
+// GET: this function is used in setting route below
+const getAllUsers = (req, res) => {
+  users
+    ? res.status(200).json({
+        status: 'Success',
+        results: users.length,
+        requestedAt: req.requestTime,
+        data: {
+          users,
+        },
+      })
+    : res.status(404).send('No users found');
+};
+
+//POST: this function will be used in 'post' method for adding something to our users data
+
+/////////////////////////////////////// GET all users //////////////////////////////////
+
+const newAllUsers = (req, res) => {
+  const updateUsers = users.map((user) => {
+    return Object.assign(user, req.body);
+  });
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/users.json`,
+    JSON.stringify(updateUsers),
+    (err) => {
+      err
+        ? res.status(40).json({
+            status: 'Invalid',
+            data: {
+              message: 'Invalid Users',
+            },
+          })
+        : res.status(200).json({
+            status: 'success',
+            data: {
+              users: updateUsers,
+            },
+          });
+    }
+  );
+};
+
+/////////////////////////////////////// get a user //////////////////////////////////
+const getUser = (req, res) => {
+  const id = req.params.id * 1;
+  const user = users[id];
+
+  id < users.length
+    ? res.status(200).json({
+        status: 'success',
+        data: {
+          user,
+        },
+      })
+    : res.status(404).json({
+        status: 'Invalid',
+        data: {
+          message: 'Invalid user',
+        },
+      });
+};
+
+/////////////////////////////////////// update user //////////////////////////////////
+
+const updateUser = (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id < users.length - 1) {
+    const user = users[id];
+    const updatedUser = Object.assign(user, (user.email = req.body));
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        updatedUser,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: 'Invalid',
+      data: {
+        message: 'Invalid Id',
+      },
+    });
+  }
+};
+
+/////////////////////////////////////// delete a user //////////////////////////////////
+
+const deleteUser = (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id < users.length - 1) {
+    const userAfterDeleting = users.filter((user) => user !== users[id]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        userAfterDeleting,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: 'Invalid',
+      data: {
+        message: 'Invalid Id',
+      },
+    });
+  }
+};
+
+// here 'route' takes the URL where a request is to be sent, and 'get' responds to the request with responce
+app.route('/api/v1/users').get(getAllUsers).post(newAllUsers); // multiple requests at same URL
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser); // with :id params
 
 const port = 3000;
 
